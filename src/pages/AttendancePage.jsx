378f4b4f76747172f141ef6d03d4b3339a64e7fc
Arnab Jana <arnabjana0099@gmail.com>
2023-12-06 23:21:25 +0530

    modify

--- src/pages/AttendancePage.jsx
+++ src/pages/AttendancePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
-import Attendance from '../components/attendance'
+import Attendance from '../components/Attendance'
 
 function AttendancePage() {
   return (
